a39a15e1e84f0650518ccc85f06292156a3d4c35
Darsh Jain <jiandrash@gmail.com>
2024-10-13 14:25:53 -0400

    update

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -3,21 +3,21 @@ import iconMapping from "./IconMapping";
 
 function Content({ data }) {
   return (
-    <main className="p-6 lg:p-6">
+    <main className="p-4 lg:p-4">
       {/* Education Section */}
       <section>
-        <h3 className="text-gray-900 mb-6">Education</h3>
-        <ul className="space-y-6">
+        <h3 className="text-gray-800 text-lg mb-4">Education</h3>
+        <ul className="space-y-4">
           {data.education.map((edu, index) => (
-            <li key={index} className="flex flex-col mb-6 p-4 border-b border-gray-200 last:border-none">
+            <li key={index} className="flex flex-col mb-4 p-3 border-b border-gray-200 last:border-none">
               <div className="flex justify-between items-center">
                 <div>
-                  <h4 className="text-2xl text-gray-800">{edu.degree}</h4>
-                  <h5 className="text-lg font-medium text-gray-600 mt-1">{edu.school}</h5>
+                  <h4 className="text-xl text-gray-700 font-normal">{edu.degree}</h4>
+                  <h5 className="text-base text-gray-500 mt-1 font-light">{edu.school}</h5>
                 </div>
                 <div className="flex flex-col text-right">
-                  <span className="text-sm text-gray-500">{edu.duration}</span>
-                  <span className="text-sm text-gray-500">GPA: {edu.gpa}</span>
+                  <span className="text-xs text-gray-400">{edu.duration}</span>
+                  <span className="text-xs text-gray-400">GPA: {edu.gpa}</span>
                 </div>
               </div>
             </li>
@@ -26,28 +26,34 @@ function Content({ data }) {
       </section>
 
       {/* Experience Section */}
-      <section className="mt-12">
-        <h3 className="text-gray-900 mb-6">Experience</h3>
-        <ul className="space-y-6">
+      <section className="mt-8">
+        <h3 className="text-gray-800 text-lg mb-4">Experience</h3>
+        <ul className="space-y-4">
           {data.experience.map((exp, index) => (
-            <li key={index} className="flex flex-col mb-6 p-4 border-b border-gray-200 last:border-none">
+            <li key={index} className="flex flex-col mb-4 p-3 border-b border-gray-200 last:border-none">
+              {/* Title and Duration */}
               <div className="flex justify-between">
-                <h4 className="text-2xl text-gray-800">{exp.role}</h4>
-                <span className="text-sm text-gray-500">{exp.duration}</span>
+                <div>
+                  <h4 className="text-xl text-gray-700 font-normal">{exp.role}</h4>
+                  <h5 className="text-base text-gray-500 font-light">{exp.company}</h5>
+                </div>
+                <span className="text-xs text-gray-400">{exp.duration}</span>
               </div>
-              <h5 className="text-lg font-medium text-gray-600 mt-1">{exp.company}</h5>
+
               {/* Tech Stack */}
-              <div className="flex flex-wrap mt-2">
+              <div className="flex flex-wrap mt-3">
                 {exp.tech_stack.split(',').map((tech, index) => (
                   <span
                     key={index}
-                    className="bg-gray-100 text-gray-600 text-sm px-2 py-1 rounded-full mr-2 mb-2"
+                    className="bg-gray-100 text-gray-500 text-xs px-2 py-1 rounded-full mr-2 mb-2"
                   >
-                    {tech}
+                    {tech.trim()}
                   </span>
                 ))}
               </div>
-              <ul className="list-disc ml-5 text-gray-600 mt-2">
+
+              {/* Description */}
+              <ul className="list-disc ml-5 text-gray-600 text-sm mt-3 space-y-1">
                 {exp.description.map((desc, idx) => (
                   <li key={idx}>{desc}</li>
                 ))}
@@ -58,19 +64,19 @@ function Content({ data }) {
       </section>
 
       {/* Projects Section */}
-      <section className="mt-12">
-        <h3 className="text-gray-900 mb-6">Projects</h3>
-        <ul className="space-y-6">
+      <section className="mt-8">
+        <h3 className="text-gray-800 text-lg mb-4">Projects</h3>
+        <ul className="space-y-4">
           {data.projects.map((project, index) => (
-            <li key={index} className="flex flex-col mb-6 p-4 border-b border-gray-200 last:border-none">
+            <li key={index} className="flex flex-col mb-4 p-3 border-b border-gray-200 last:border-none">
               <div className="flex justify-between items-center">
-                <h4 className="text-2xl text-gray-800">{project.name}</h4>
-                <span className="text-sm text-gray-500">{project.duration}</span>
+                <h4 className="text-xl text-gray-700 font-normal">{project.name}</h4>
+                <span className="text-xs text-gray-400">{project.duration}</span>
               </div>
-              <p className="text-gray-600 mt-2">{project.description}</p>
+              <p className="text-gray-600 text-sm mt-2">{project.description}</p>
 
               {/* Icons for links (Patent, Paper, GitHub) */}
-              <div className="mt-2 flex space-x-4">
+              <div className="mt-2 flex space-x-3">
                 {project.patent && (
                   <a
                     href={project.patent}
@@ -82,7 +88,7 @@ function Content({ data }) {
                     <img
                       src={iconMapping("Patent")}
                       alt="Patent"
-                      className="w-6 h-6"
+                      className="w-5 h-5"
                     />
                   </a>
                 )}
@@ -98,7 +104,7 @@ function Content({ data }) {
                     <img
                       src={iconMapping("Paper")}
                       alt="Paper"
-                      className="w-6 h-6"
+                      className="w-5 h-5"
                     />
                   </a>
                 )}
@@ -114,7 +120,7 @@ function Content({ data }) {
                     <img
                       src={iconMapping("Github")}
                       alt="GitHub"
-                      className="w-6 h-6"
+                      className="w-5 h-5"
                     />
                   </a>
                 )}
@@ -125,15 +131,15 @@ function Content({ data }) {
       </section>
 
       {/* Skills Section */}
-      <section className="mt-12">
-        <h3 className="text-gray-900 mb-6 text-center sm:text-left">
+      <section className="mt-8">
+        <h3 className="text-gray-800 text-lg mb-4 text-center sm:text-left">
           Skills
         </h3>
-        <ul className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-8 gap-8">
+        <ul className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-8 gap-6">
           {data.skills.map((skill, index) => (
             <li
               key={index}
-              className="bg-gray-100 p-6 shadow-md rounded-lg text-center flex flex-col items-center justify-center h-24 group hover:shadow-lg transition-shadow duration-300"
+              className="bg-gray-100 p-4 shadow-md rounded-lg text-center flex flex-col items-center justify-center h-20 group hover:shadow-lg transition-shadow duration-300"
             >
               <div className="flex flex-col items-center justify-center">
                 {iconMapping(skill) ? (
@@ -141,14 +147,14 @@ function Content({ data }) {
                     <img
                       src={iconMapping(skill)}
                       alt={skill}
-                      className="w-15 h-12 mb-2 group-hover:animate-spin"
+                      className="w-10 h-8 mb-2 group-hover:animate-spin"
                     />
-                    <h4 className="text-sm text-gray-700">
+                    <h4 className="text-xs text-gray-700 font-normal">
                       {skill}
                     </h4>
                   </>
                 ) : (
-                  <h4 className="text-sm text-gray-700">
+                  <h4 className="text-xs text-gray-700 font-normal">
                     {skill}
                   </h4>
                 )}
